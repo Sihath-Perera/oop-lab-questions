bf9c5549fe2e5308072f2045128f245a84345ae2
SihathPerera <82312993+Sihath-Perera@users.noreply.github.com>
2026-04-16 13:58:04 +0530

    Create Project Project File

---
+++ OOP-Github-Collab/src/main/java/com/mycompany/oop/github/collab/OOPGithubCollab.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oop.github.collab;
+
+/**
+ *
+ * @author Sihath
+ */
+public class OOPGithubCollab {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
