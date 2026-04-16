91fea4b5a028cac520cce04d401485a2cc5af506
SihathPerera <82312993+Sihath-Perera@users.noreply.github.com>
2026-04-16 14:13:26 +0530

    lab04 q5 static members added

--- OOP-Github-Collab/src/main/java/com/mycompany/oop/github/collab/StaffMember.java
+++ OOP-Github-Collab/src/main/java/com/mycompany/oop/github/collab/StaffMember.java
@@ -13,13 +13,21 @@ public abstract class StaffMember {
     private String fullName; 
     private final String staffId;
     protected String department; 
-
+    private static int staffCount = 0;
+    
     public StaffMember(String fullName, String staffId, String department) {
         this.fullName = fullName;
         this.staffId = staffId;
         this.department = department;
+        staffCount++;
     }
-
+    public static void showSystemName() {
+        System.out.println("Campus Staff Payment System");
+    }
+    public static int getStaffCount() {
+        return staffCount;
+    }
+    
     public String getFullName() { return fullName; }
     public String getStaffId() { return staffId; }
     public String getDepartment() { return department; }
